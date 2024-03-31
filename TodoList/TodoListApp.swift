46e12c4b2a5b3ed962a5142ffeb0ce7c434165a4
Manan <bhatiamanan4@gmail.com>
2024-03-31 15:25:24 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Manan Bhatia on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
